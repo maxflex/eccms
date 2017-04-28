621583f0bd600b70704d900795f99052c35a8ffb
Max Kolyadin <makcyxa-k@yandex.ru>
2017-04-28 14:37:56 +0300

    Sync 2.0 rc

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -215,3 +215,8 @@
     function mb_ucfirst($text) {
         return mb_strtoupper(mb_substr($text, 0, 1)) . mb_substr($text, 1);
     }
+
+    function firstLine($s)
+    {
+        return explode("\n", $s)[0];
+    }
