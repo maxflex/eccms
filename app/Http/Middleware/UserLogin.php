8e26c1b316452fac465012e78c862224a56fff6e
maxflex <makcyxa-k@yandex.ru>
2018-07-10 15:18:56 +0300

    Login screen update

--- app/Http/Middleware/UserLogin.php
+++ app/Http/Middleware/UserLogin.php
@@ -24,7 +24,7 @@ class UserLogin
                 // если не найден, делаем dummy-объект с зеленым фоном
                 if ($wallpaper === null) {
                     $wallpaper = (object)[
-                        'image_url' => 'img/background/green.png'
+                        'image_url' => config('app.egerep-url') . Background::UPLOAD_DIR . 'blue.png'
                     ];
                 }
             }
